fcf44d753b5128f4d54165caa1bfbdf1439cb5ef
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2019-01-15 09:48:42 +0100

    added license

---
+++ src/pruebadeblinux/PruebaDebLinux.java
@@ -0,0 +1,12 @@
+package pruebadeblinux;
+
+import javax.swing.JOptionPane;
+
+public class PruebaDebLinux {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola mundo :)", "Aplicación", 3);
+        System.exit(0);
+    }
+    
+}
